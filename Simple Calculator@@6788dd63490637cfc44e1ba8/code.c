2b1b0ebc42eebde26f770e0b1568cb1ec4cf8523
Kushal Sachdeva <kushal1362.becse24@chitkara.edu.in>
2025-03-15 19:03:01 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,24 @@
+// Your code here...
+#include <stdio.h>
+int main()
+{
+    int x,y;
+    char ch;
+    scanf("%d %d %d",&x,&y, &ch);
+    int s = x+y;
+    int d = x-y;
+    int p = x*y;
+    int di = x/y;
+    if(ch=='+'){
+        printf("%d",s);
+    }
+    else if(ch=='-'){
+        printf("%d",d);
+    }
+    else if(ch=='*'){
+        printf("%d",p);
+    }
+    else if(ch=='/'){
+        printf("%d",di);
+    }
+}
